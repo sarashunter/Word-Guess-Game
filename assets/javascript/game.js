25c7cb7db35faf746c651da7265dadd551561aa5
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-07-14 14:43:21 -0400

    Add resetVariables function and Game Tally

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,48 @@
-var wins = 0;
-var words = ["cat", "dog"]; //These are possible words.
-var lettersGuess = []; //These are the letters the user has guessed.
-var guesses = 5; //This is a counter for the number of guesses.  It counts down to 0.
-var alreadyGuessed = [];
-var correctGuesses = 0;
+    var words = ["cat", "dog", "banana"]; //These are possible words.
 
-var chosenWord = words[Math.floor(Math.random() * words.length)]; //Gives a random word
-var chosenWordArray = chosenWord.split('');
-console.log(chosenWordArray);
+    var chosenWord; //Holds the current word
+    var chosenWordArray; //Holds the current word as an array of characters;
 
-alreadyGuessed.length = chosenWordArray.length;
-console.log("The length of your array is " + alreadyGuessed.length);
-alreadyGuessed.fill("_");
+    var guesses; //This is a counter for the number of guesses.  It counts down to 0.
+    var correctGuesses;//This is the number of correct guesses by the user.  It gets compared to the length.
+    var currentState; //This is an array of the underscores and correct letters guessed by the user
+    var lettersGuessed; //This is an array that holds the letters guessed so far (correct and incorrect)
 
-var lettersGuessed = [""];
+    var wins=0;
+    var losses=0;
 
+    var guessedWordDiv = document.getElementById("guessedWord");
+    guessedWordDiv.textContent = currentState;
+
+    var guessesDiv = document.getElementById("guessesRemaining");
+    guessesDiv.textContent=guesses;
+
+    var tallyDiv = document.getElementById("runningTally");
+    tallyDiv.textContent=("Wins: "+wins + " Losses: " + losses);
+
+    // document.querySelector("#guessedWord").innerHTML="help";
+
+    var resetVariables = function(){
+        lettersGuessed=[""];
+        console.log("The letters already guessed are "+lettersGuessed);
+        guesses=5;
+        currentState=[];
+        correctGuesses=0;
+        chosenWord=words[Math.floor(Math.random() * words.length)]; //Gives a random word
+        chosenWordArray = chosenWord.split('');
+        currentState.length = chosenWordArray.length;   
+        currentState.fill("_");
+        guessedWordDiv.textContent = currentState;
+        guessesDiv.textContent=guesses;
+        console.log("The new word is " + chosenWordArray);
+        tallyDiv.textContent=("Wins: "+wins + " Losses: " + losses);
+}
+
+    // function startGame{
+
+
+    // }
+resetVariables();
     document.onkeyup = function (event) {
 
         // Determines which key was pressed.
@@ -26,33 +54,51 @@ var lettersGuessed = [""];
 
         //First check if the letter has already been guessed.
         if (lettersGuessed.indexOf(userGuess) !== -1) {
+
             //If the letter has been guessed, tell the user.  Wait for next letter.
-            console.log("You already guessed that.");
+            console.log("You already guessed that. lettersGuessed is "+lettersGuessed + "user guess is + " + userGuess);
+
         } else {
+
             //If the letter is a new letter, first check if it's part of the word.
+
             var indexGuess = chosenWord.indexOf(userGuess);
+
             if (indexGuess === -1) {
                 //If guess is not part of word, take away a guess.
                 guesses = guesses - 1;
+                guessesDiv.textContent=guesses;
                 console.log("Guesses left: " + guesses);
                 console.log("Wrong");
+                lettersGuessed.push(userGuess);
+
             } else {
                 //The letter is part of the word.
-                alreadyGuessed[indexGuess] = userGuess;
-                console.log(lettersGuess);
-                console.log(alreadyGuessed);
-                correctGuesses++;
-                if (correctGuesses===chosenWord.length){
-                    console.log("Winner ")
+
+                //Need to check if the letter occurs more than once.
+                for (var i=0; i<chosenWord.length; i++){
+                    if (chosenWord[i]===userGuess){
+                        currentState[i]=userGuess;
+                        correctGuesses++;
+                        lettersGuessed.push(userGuess);
+                    }
+                }
+                guessedWordDiv.textContent = currentState;
+
+                console.log(currentState);
+
+                if (correctGuesses === chosenWord.length) {
+                    guessedWordDiv.textContent="Winner";
+                    wins++;
+                    resetVariables();
                 }
             }
-
-            //Add guess to already guessed.
-            lettersGuessed.push(userGuess);
 
             //Check if the user is out of guesses.
             if (guesses === 0) {
                 console.log("You've lost.");
+                losses++;
+                resetVariables();
             }
         }
     }
